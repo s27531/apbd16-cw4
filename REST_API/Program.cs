84e54bc3adda56a896ad59fa7134a55f9646bcdf
s27531 <s27531@pjwstk.edu.pl>
2025-04-18 14:58:30 +0200

    chore: Create project

---
+++ REST_API/Program.cs
@@ -0,0 +1,28 @@
+namespace REST_API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+        
+        app.Run();
+    }
+}
