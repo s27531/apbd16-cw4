45098cae37dbb15734baf21430331ed1fd7a30c5
s27531 <s27531@pjwstk.edu.pl>
2025-04-18 15:03:06 +0200

    feat: Add Animal.cs

---
+++ REST_API/Models/Animal.cs
@@ -0,0 +1,9 @@
+namespace REST_API.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Colour { get; set; }
+}
